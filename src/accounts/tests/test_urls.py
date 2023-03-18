9e0e2e4ef7ee9acd7eb5d2b85127f1aed53920bb
Oleksandr Petkov <sasha111108@gmail.com>
2023-03-18 09:31:38 +0200

    add tests from class work

---
+++ src/accounts/tests/test_urls.py
@@ -0,0 +1,18 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from accounts.views import UserRegisterView, user_profile_view, user_activate
+
+
+class TestUrls(SimpleTestCase):
+    def test_register_url_resolves(self):
+        url = reverse('accounts:register')
+        self.assertEqual(resolve(url).func.view_class, UserRegisterView)
+
+    def test_profile_url_resolves(self):
+        url = reverse('accounts:profile')
+        self.assertEqual(resolve(url).func, user_profile_view)
+
+    def test_activate_user_url_resolves(self):
+        url = reverse('accounts:register_activate', kwargs={'sign': 'yrtrrd654df65dsf'})
+        self.assertEqual(resolve(url).func, user_activate)
