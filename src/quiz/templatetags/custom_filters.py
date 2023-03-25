3c852746b191dbb374a60806a67e5878cb915eaf
Oleksandr Petkov <sasha111108@gmail.com>
2023-03-25 11:20:28 +0200

    add point value

--- src/quiz/templatetags/custom_filters.py
+++ src/quiz/templatetags/custom_filters.py
@@ -16,6 +16,14 @@ def dived(value, arg):
     return value // arg
 
 
+def get_point_sum(correct, incorrect):
+    point = correct - incorrect
+    if point > 0:
+        return point
+    return 0
+
+
 register.filter('negative', negative)
 register.filter('multi', multi)
 register.filter('dived', dived)
+register.filter('get_ppoint_sum', get_point_sum)
